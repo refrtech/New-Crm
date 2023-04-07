89b2e9074f39607e2fe64c9cd3548af2e4923151
refrtech <getrefr.in@gmail.com>
2023-04-07 17:27:47 +0530

    image kit changes

--- src/app/components/clientside/nodemanagement-citys/nodemanagement-citys.component.ts
+++ src/app/components/clientside/nodemanagement-citys/nodemanagement-citys.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { ApiserviceService } from 'src/app/apiservice.service';
@@ -6,53 +6,40 @@ import { ApiserviceService } from 'src/app/apiservice.service';
 @Component({
   selector: 'app-nodemanagement-citys',
   templateUrl: './nodemanagement-citys.component.html',
-  styleUrls: ['./nodemanagement-citys.component.scss']
+  styleUrls: ['./nodemanagement-citys.component.scss'],
 })
 export class NodemanagementCitysComponent implements OnInit {
   cityList$: Observable<any[]> = of();
-  fileName:string = '';
+  fileName: string = '';
 
-
-  constructor(
-    public api: ApiserviceService,
-    public http: HttpClient
-  ) { }
+  constructor(public api: ApiserviceService, public http: HttpClient) {}
 
   ngOnInit(): void {
     this.getallcity();
   }
 
   getallcity() {
-    this.cityList$= this.api.getcity();
+    this.cityList$ = this.api.getcity();
   }
 
-
-
-
-
-
-
-
-  cloudupload2(event: any){
+  cloudupload2(event: any) {
     const file = event.target.files && event.target.files[0];
     if (file) {
       var reader = new FileReader();
       reader.readAsDataURL(file);
       this.fileName = file.name;
 
-    const formData = new FormData();
-    formData.append('file', file, file.name);
-    const url = 'https://api.imagekit.io/v1/files/upload';
-    this.http
-      .post(url, formData, {
-        headers: {
-          Authorization: 'private_u8LLcJelqtJeixtvSTyJ9VYH82Q=',
-          'Content-Type': 'multipart/form-data',
-        },
-      })
-      .subscribe(
+      const formData = new FormData();
+      formData.append('file', file, file.name);
+
+      const headers = new HttpHeaders();
+      headers.append('Content-Type:multipart/form-data; boundary=------------------------1234567890', 'multipart/form-data');
+
+      const url =
+        'https://us-central1-refr-india.cloudfunctions.net/ind_serve/api/imagekit/upload';
+      this.http.post(url, formData, {headers}).subscribe(
         (res) => {
-          console.log("success");
+          console.log('success');
           console.log(res);
         },
         (err) => {
